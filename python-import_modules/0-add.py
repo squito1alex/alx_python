b77ddd5a5382920c2072dd00a36db995a69eb54d
squito1alex <ejorucalex@gmail.com>
2023-08-08 04:48:40 -0700

    add function

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+a = 1
+b = 2
+def add(a, b):
+    """My addition function
+    Args:
+    a: first integer
+    b: second integer
+    Returns:
+    The return value. a + b
+    """
+    return (a + b)
+
+result = add(a, b)
+print(f"{a} + {b} = {result}")
+
