b6900af47f6facd49f29f5023435f4d5d8861f28
squito1alex <ejorucalex@gmail.com>
2023-08-08 05:15:14 -0700

    additions

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-
-from add_0 import add
-
-result = add(a, b)
-print(f"{a} + {b} = {result}")
+def add (a, b):
+    a = 1
+    b = 2
+    from add_0 import add
+    result = add(a, b)
+    print(f"{a} + {b} = {result}")
 
