52b918d98829c5db5ae8ea604c3e24ce7409059c
squito1alex <ejorucalex@gmail.com>
2023-08-08 06:05:35 -0700

    argument

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-
-args = sys.argv[1:]
-num_args = len(args)
-
-if num_args == 0:
+    args = sys.argv[1:]
+    num_args = len(args)
+    if num_args == 0:
         print("0 arguments.")
     elif num_args == 1:
-            print("1 argument:")
-        else:
-                print("{} arguments:".format(num_args))
-
-                for i, arg in enumerate(args, start=1):
-                        print("{}: {}".format(i, arg))
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num_args))
+        for i, arg in enumerate(args, start=1):
+            print("{}: {}".format(i, arg))
 
